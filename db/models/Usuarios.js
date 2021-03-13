b1301e03f17ea5cfd2e29432c99a90c396300f40
Christian Ontiveros <contiverosv@outlook.com>
2021-03-13 14:51:07 -0600

    Terminamos esquema de Usuario

--- db/models/Usuarios.js
+++ db/models/Usuarios.js
@@ -1,6 +1,41 @@
 const { Schema, model } = require("mongoose");
 const bcrypt = require("bcryptjs");
 
+const experienciaLaboral = new Schema(
+  {
+    empresa: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    ingreso: {
+      type: Date,
+      required: true,
+    },
+    egreso: {
+      type: Date,
+    },
+    puesto: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    descipcionActividades: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    permaneceTrabajando: {
+      type: boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
 const UsuarioSchema = new Schema(
   {
     nombre: {
@@ -47,7 +82,68 @@ const UsuarioSchema = new Schema(
         required: true,
         trim: true,
       },
+      unidadAcademica: {
+        type: String,
+        required: true,
+        trim: true,
+      },
     },
+    contacto: [
+      {
+        contact: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+        contactMethod: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+      },
+    ],
+    trayectoriaAcademica: [
+      {
+        escuela: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+        ingreso: {
+          type: Date,
+          required: true,
+        },
+        egreso: {
+          type: Date,
+        },
+        nivel: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+      },
+    ],
+    trayectoriaLaboral: [
+      {
+        detalles: experienciaLaboral,
+      },
+    ],
+    servicioSocial: {
+      type: boolean,
+      default: false,
+    },
+    practicasProfesionales: {
+      type: boolean,
+      default: false,
+    },
+    busquedas: [
+      {
+        query: {
+          type: String,
+          trim: true,
+        },
+      },
+    ],
   },
   {
     timestamps: true,
